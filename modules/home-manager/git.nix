cf58609dea5fac2c0f36c7a73365f4089f9efd9b
Harley Watson <htw@lobi.to>
2024-01-16 01:18:11 -0800

    git config

---
+++ modules/home-manager/git.nix
@@ -0,0 +1,27 @@
+{pkgs, ...}: {
+  programs.git = {
+    enable = true;
+    userName = "Harley Watson";
+    userEmail = "htw@lobi.to";
+    lfs.enable = true;
+    extraConfig = {
+      init.defaultBranch = "main";
+      pull.rebase = false;
+      push.autoSetupRemote = true;
+      includeIf = {
+        "gitdir:~/w/skyscanner/" = {
+          path = "config-skyscanner.inc";
+        };
+      };
+    };
+  };
+
+
+  home.file = {
+    ".config/git/config-skyscanner.inc".text = ''
+    [user]
+      name = Harley Watson
+      email = harley.watson@skyscanner.net
+    '';
+  };
+}
