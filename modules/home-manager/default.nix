0a569c9016a24c43de7e058312aa4a917ac211e5
Harley Watson <htw@lobi.to>
2024-03-25 22:52:21 +0000

    add Justfile for formatting

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -21,7 +21,10 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    just
+    nvd
     nixpkgs-fmt
+
     asdf-vm
   ];
 
