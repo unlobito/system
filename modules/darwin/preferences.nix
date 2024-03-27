fabcc3cce645e476ab83d29cea88fd011c944c30
Harley Watson <htw@lobi.to>
2024-03-27 21:58:18 +0000

    darwin: declare dock/finder/menu/screensaver preferences

--- modules/darwin/preferences.nix
+++ modules/darwin/preferences.nix
@@ -5,11 +5,29 @@
     dock = {
       autohide = true;
       mru-spaces = false;
+
+      wvous-bl-corner = 13; # Lock Screen
+      wvous-br-corner = 1; # Disabled
+      wvous-tl-corner = 1; # Disabled
+      wvous-tr-corner = 1; # Disabled
     };
 
     finder = {
       AppleShowAllExtensions = true;
       FXPreferredViewStyle = "clmv";
+      ShowPathbar = true;
+      ShowStatusBar = true;
+    };
+
+    menuExtraClock = {
+      Show24Hour = true;
+      ShowDate = 0;
+      ShowDayOfWeek = true;
+      ShowSeconds = true;
+    };
+
+    screensaver = {
+      askForPassword = true;
     };
   };
 }
