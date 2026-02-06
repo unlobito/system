2731b08704e592f8dbf4b8a750a9caf1a3cb0c7c
Harley Watson <htw@lobi.to>
2026-02-06 00:40:13 +0000

    thonkpad: plasma5 -> plasma6

--- modules/nixos/graphical.nix
+++ modules/nixos/graphical.nix
@@ -12,7 +12,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -24,7 +24,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
